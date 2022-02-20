a178f3ef490bee882989988b037eff291bdfe85e
Raneem-Mohamed <raneemm50@gmail.com>
2022-02-20 06:45:28 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortoComponent } from './porto/porto.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    PortoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
